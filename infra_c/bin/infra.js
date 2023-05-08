e4e8ff0d52764b88614e2d240df9e7fdb2ccfe0e
visdxebia <vishal.daga@xebia.com>
2023-05-08 13:11:23 +0530

    Sdf

--- infra_c/bin/infra.js
+++ infra_c/bin/infra.js
@@ -5,16 +5,16 @@ const krnySnpApplicationStack = require('../lib/krny-snp-application-stack');
 const fs = require("fs");
 const path = require("path");
 
-// const cdkConfig = process.env.cdkConfig;
-
-const cdkConfig = {
-  awsAccount: 932399466203,
-  region: 'us-east-1',
-  envName: 'dev',
-  certificateArn: "arn:aws:acm:us-east-1:932399466203:certificate/f085089d-f5ab-4286-8feb-08cac18e208e",
-  domain: "trial.dev.testvisd.online",
-  hostedZoneId: "Z05023842CSJVZ3JVYYYJ"
-}
+const cdkConfig = JSON.parse(process.env.cdkClientOnboardingConfig);
+console.log("cdkConfig", cdkConfig, JSON.stringify(cdkConfig), cdkConfig.awsAccount)
+//
+// const cdkConfig = {
+//   "awsAccount": "932399466203",
+//   "region": "us-east-1",
+//   "envName": "dev",
+//   "certificateArn": "arn:aws:acm:us-east-1:932399466203:certificate/f085089d-f5ab-4286-8feb-08cac18e208e",
+//   "domain": "trial.dev.testvisd.online",
+// }
 
 const scannedClientTable = fs.readFileSync(path.join(__dirname, './scannedClientTable.json'), "utf-8")
 const clientsToOnboardConfigs = JSON.parse(scannedClientTable);
