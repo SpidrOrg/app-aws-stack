649656c7fe4c9e969131689ec8cded9d810a7e6c
visdxebia <vishal.daga@xebia.com>
2023-05-09 22:08:34 +0530

    df

--- infra_c/bin/infra.js
+++ infra_c/bin/infra.js
@@ -5,17 +5,17 @@ const krnySnpApplicationStack = require('../lib/krny-snp-application-stack');
 const fs = require("fs");
 const path = require("path");
 
-// let envVal = process.env.clientonboardingconfig;
-// envVal = envVal.replaceAll("'", "");
-// const cdkConfig = JSON.parse(`${envVal}`);
+let envVal = process.env.clientonboardingconfig;
+envVal = envVal.replaceAll("'", "");
+const cdkConfig = JSON.parse(`${envVal}`);
 //
-const cdkConfig = {
-  "awsAccount": "932399466203",
-  "region": "us-east-1",
-  "envName": "dev",
-  "certificateArn": "arn:aws:acm:us-east-1:932399466203:certificate/f085089d-f5ab-4286-8feb-08cac18e208e",
-  "domain": "trial.dev.testvisd.online",
-}
+// const cdkConfig = {
+//   "awsAccount": "932399466203",
+//   "region": "us-east-1",
+//   "envName": "dev",
+//   "certificateArn": "arn:aws:acm:us-east-1:932399466203:certificate/f085089d-f5ab-4286-8feb-08cac18e208e",
+//   "domain": "trial.dev.testvisd.online",
+// }
 
 const scannedClientTable = fs.readFileSync(path.join(__dirname, './scannedClientTable.json'), "utf-8")
 const clientsToOnboardConfigs = JSON.parse(scannedClientTable);
