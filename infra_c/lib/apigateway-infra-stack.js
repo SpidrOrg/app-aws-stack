d822b008e0628e0c3b361b4815fdf6e82262d2f3
visdxebia <vishal.daga@xebia.com>
2023-05-11 04:24:58 +0530

    sdf

--- infra_c/lib/apigateway-infra-stack.js
+++ infra_c/lib/apigateway-infra-stack.js
@@ -80,7 +80,7 @@ class ApiGatewayInfraStack extends Stack {
                 {
                   statusCode: "200",
                   responseTemplates: {
-
+                    "application/json": apigateway.Model.EMPTY_MODEL
                   }
                 },
               ],
