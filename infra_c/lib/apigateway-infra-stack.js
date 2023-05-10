75d58194240dd58a69842a566f65de510b25d2d7
visdxebia <vishal.daga@xebia.com>
2023-05-10 12:53:02 +0530

    sdf

--- infra_c/lib/apigateway-infra-stack.js
+++ infra_c/lib/apigateway-infra-stack.js
@@ -90,54 +90,53 @@ class ApiGatewayInfraStack extends Stack {
     });
 
 
+    const pathToDefaultDashboardBundle = path.join(__dirname, "../../services/DefaultUiBundle");
+    const POOLID_PALCEHOLDER = ":POOLID:";
+    const POOLWEBCLIENTID_PALCEHOLDER = ":POOLWEBCLIENTID:";
+    const POOLOAUTHDOMAIN_PALCEHOLDER = ":POOLOAUTHDOMAIN:";
+    const POOLREDIRECTSIGNIN_PALCEHOLDER = ":POOLREDIRECTSIGNIN:";
+    const POOLREDIRECTSIGNOUT_PALCEHOLDER = ":POOLREDIRECTSIGNOUT:";
+    const APIPREFIX_PALCEHOLDER = ":APIPREFIX:";
+    const APISTAGE_PALCEHOLDER = ":APISTAGE:";
+    const CLIENTID_PALCEHOLDER = ":CLIENTID:";
+    const CLIENTDISPLAYNAME_PALCEHOLDER = ":CLIENTDISPLAYNAME:";
 
-    // const pathToDefaultDashboardBundle = path.join(__dirname, "../../services/DefaultUiBundle");
-    // const POOLID_PALCEHOLDER = ":POOLID:";
-    // const POOLWEBCLIENTID_PALCEHOLDER = ":POOLWEBCLIENTID:";
-    // const POOLOAUTHDOMAIN_PALCEHOLDER = ":POOLOAUTHDOMAIN:";
-    // const POOLREDIRECTSIGNIN_PALCEHOLDER = ":POOLREDIRECTSIGNIN:";
-    // const POOLREDIRECTSIGNOUT_PALCEHOLDER = ":POOLREDIRECTSIGNOUT:";
-    // const APIPREFIX_PALCEHOLDER = ":APIPREFIX:";
-    // const APISTAGE_PALCEHOLDER = ":APISTAGE:";
-    // const CLIENTID_PALCEHOLDER = ":CLIENTID:";
-    // const CLIENTDISPLAYNAME_PALCEHOLDER = ":CLIENTDISPLAYNAME:";
-    //
-    // // Dashboards Bucket
-    // const dashboardsBucketARN = Fn.importValue(`dashboardsBucketARN`);
-    // const dashboardsBucket = s3.Bucket.fromBucketArn(this, `dashboardsBundleBucket`, dashboardsBucketARN);
-    // allEntities.forEach(entity =>{
-    //   const clientId = entity.id;
-    //   const entityName = entity.name;
-    //
-    //   // Cognito Stack Imports
-    //   const cognitoUserPoolId = Fn.importValue(`userpool${clientId}ResourceIdsUserPoolId`);
-    //   const cognitoUserPoolWebClient = Fn.importValue(`userpool${clientId}ResourceIdsClientId`);
-    //   const cognitoUserPoolDomain = Fn.importValue(`userpool${clientId}ResourceIdsDomain`);
-    //   const cognitoUserPoolRedirectSignInPublicUrl = Fn.importValue(`userpool${clientId}redirectSignIn`);
-    //   const cognitoUserPoolRedirectSignOutPublicUrl = Fn.importValue(`userpool${clientId}redirectSignOut`);
-    //
-    //   const idpConfigFilePath = path.join(pathToDefaultDashboardBundle, "idpConfig.js");
-    //   let idpConfig = fs.readFileSync(idpConfigFilePath, "utf-8");
-    //   idpConfig = idpConfig.replaceAll(POOLID_PALCEHOLDER, `${cognitoUserPoolId}`);
-    //   idpConfig = idpConfig.replaceAll(POOLWEBCLIENTID_PALCEHOLDER, `${cognitoUserPoolWebClient}`);
-    //   idpConfig = idpConfig.replaceAll(POOLOAUTHDOMAIN_PALCEHOLDER, `${cognitoUserPoolDomain}`);
-    //   idpConfig = idpConfig.replaceAll(POOLREDIRECTSIGNIN_PALCEHOLDER, `${cognitoUserPoolRedirectSignInPublicUrl}`);
-    //   idpConfig = idpConfig.replaceAll(POOLREDIRECTSIGNOUT_PALCEHOLDER, `${cognitoUserPoolRedirectSignOutPublicUrl}`);
-    //
-    //   idpConfig = idpConfig.replaceAll(APIPREFIX_PALCEHOLDER, `${api.url}`);
-    //   idpConfig = idpConfig.replaceAll(APISTAGE_PALCEHOLDER, `${api.deploymentStage.stageName}`);
-    //   idpConfig = idpConfig.replaceAll(CLIENTID_PALCEHOLDER, `${clientId}`);
-    //   idpConfig = idpConfig.replaceAll(CLIENTDISPLAYNAME_PALCEHOLDER, `${entityName}`);
-    //   fs.writeFileSync(path.join(pathToDefaultDashboardBundle, "idpConfig.js"), idpConfig);
-    //
-    //   const bucketDeployment = new s3deploy.BucketDeployment(this, `dashboard-bundler-${clientId}`, {
-    //     sources: [s3deploy.Source.asset(pathToDefaultDashboardBundle)],
-    //     destinationBucket: dashboardsBucket,
-    //     destinationKeyPrefix: `dashboards/${clientId}/dashboard`
-    //   });
-    //
-    //   bucketDeployment.node.addDependency(api);
-    // })
+    // Dashboards Bucket
+    const dashboardsBucketARN = Fn.importValue(`dashboardsBucketARN`);
+    const dashboardsBucket = s3.Bucket.fromBucketArn(this, `dashboardsBundleBucket`, dashboardsBucketARN);
+    allEntities.forEach(entity =>{
+      const clientId = entity.id;
+      const entityName = entity.name;
+
+      // Cognito Stack Imports
+      const cognitoUserPoolId = Fn.importValue(`userpool${clientId}ResourceIdsUserPoolId`);
+      const cognitoUserPoolWebClient = Fn.importValue(`userpool${clientId}ResourceIdsClientId`);
+      const cognitoUserPoolDomain = Fn.importValue(`userpool${clientId}ResourceIdsDomain`);
+      const cognitoUserPoolRedirectSignInPublicUrl = Fn.importValue(`userpool${clientId}redirectSignIn`);
+      const cognitoUserPoolRedirectSignOutPublicUrl = Fn.importValue(`userpool${clientId}redirectSignOut`);
+
+      const idpConfigFilePath = path.join(pathToDefaultDashboardBundle, "idpConfig.js");
+      let idpConfig = fs.readFileSync(idpConfigFilePath, "utf-8");
+      idpConfig = idpConfig.replaceAll(POOLID_PALCEHOLDER, `${cognitoUserPoolId}`);
+      idpConfig = idpConfig.replaceAll(POOLWEBCLIENTID_PALCEHOLDER, `${cognitoUserPoolWebClient}`);
+      idpConfig = idpConfig.replaceAll(POOLOAUTHDOMAIN_PALCEHOLDER, `${cognitoUserPoolDomain}`);
+      idpConfig = idpConfig.replaceAll(POOLREDIRECTSIGNIN_PALCEHOLDER, `${cognitoUserPoolRedirectSignInPublicUrl}`);
+      idpConfig = idpConfig.replaceAll(POOLREDIRECTSIGNOUT_PALCEHOLDER, `${cognitoUserPoolRedirectSignOutPublicUrl}`);
+
+      idpConfig = idpConfig.replaceAll(APIPREFIX_PALCEHOLDER, `${api.url}`);
+      idpConfig = idpConfig.replaceAll(APISTAGE_PALCEHOLDER, `${api.deploymentStage.stageName}`);
+      idpConfig = idpConfig.replaceAll(CLIENTID_PALCEHOLDER, `${clientId}`);
+      idpConfig = idpConfig.replaceAll(CLIENTDISPLAYNAME_PALCEHOLDER, `${entityName}`);
+      fs.writeFileSync(path.join(pathToDefaultDashboardBundle, "idpConfig.js"), idpConfig);
+
+      const bucketDeployment = new s3deploy.BucketDeployment(this, `dashboard-bundler-${clientId}`, {
+        sources: [s3deploy.Source.asset(pathToDefaultDashboardBundle)],
+        destinationBucket: dashboardsBucket,
+        destinationKeyPrefix: `dashboards/${clientId}/dashboard`
+      });
+
+      bucketDeployment.node.addDependency(api);
+    })
   }
 }
 
