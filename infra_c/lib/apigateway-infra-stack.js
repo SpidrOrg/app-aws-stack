6f286de128751083517266475feb16e967433e17
visdxebia <vishal.daga@xebia.com>
2023-05-11 01:50:27 +0530

    sdf

--- infra_c/lib/apigateway-infra-stack.js
+++ infra_c/lib/apigateway-infra-stack.js
@@ -18,18 +18,7 @@ class ApiGatewayInfraStack extends Stack {
       description: 'Created by CDK',
       deployOptions: {
         stageName: envName,
-      },
-      defaultCorsPreflightOptions: {
-        allowHeaders: [
-          'Content-Type',
-          'X-Amz-Date',
-          'Authorization',
-          'X-Api-Key',
-        ],
-        allowMethods: ['OPTIONS', 'GET', 'POST'],
-        allowCredentials: true,
-        allowOrigins: ['*'],
-      },
+      }
     });
 
     new CfnOutput(this, `apiGatewayRootUrl`, {
@@ -86,9 +75,30 @@ class ApiGatewayInfraStack extends Stack {
             }
           );
         })
-        resource.addCorsPreflight({
-          allowOrigins: apigateway.Cors.ALL_ORIGINS,
-          allowMethods: apigateway.Cors.ALL_METHODS,
+        resource.addMethod('OPTIONS', new apigateway.MockIntegration({
+          integrationResponses: [{
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+              'method.response.header.Access-Control-Allow-Origin': "'*'",
+              'method.response.header.Access-Control-Allow-Credentials': "'false'",
+              'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+            },
+          }],
+          passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+          requestTemplates: {
+            "application/json": "{\"statusCode\": 200}"
+          },
+        }), {
+          methodResponses: [{
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers': true,
+              'method.response.header.Access-Control-Allow-Methods': true,
+              'method.response.header.Access-Control-Allow-Credentials': true,
+              'method.response.header.Access-Control-Allow-Origin': true,
+            },
+          }]
         })
       })
     });
