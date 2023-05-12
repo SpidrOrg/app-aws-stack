b88ce5bcb3c7c8d3c42e5c95d19399f5d6aef362
visdxebia <vishal.daga@xebia.com>
2023-05-13 00:33:12 +0530

    sdf

--- infra_c/bin/infra.js
+++ infra_c/bin/infra.js
@@ -28,10 +28,14 @@ const path = require("path");
 const scannedClientTable = fs.readFileSync(path.join(__dirname, './scannedClientTable.json'), "utf-8")
 const clientsToOnboardConfigs = JSON.parse(scannedClientTable);
 
-const awsAccount = process.env.CODEBUILD_BUILD_ARN.split(":")[1];
+// This
+const indexOfAwsAccountInArnSplit = process.env.CODEBUILD_BUILD_ARN.split(":").indexOf(process.env.AWS_REGION) + 1;
+const awsAccount = process.env.CODEBUILD_BUILD_ARN.split(":")[indexOfAwsAccountInArnSplit];
 const awsRegion = process.env.AWS_REGION;
+// or this//
 // const awsAccount = "932399466203";
 // const awsRegion = "us-east-1";
+//
 
 const app = new cdk.App();
 const props = {
