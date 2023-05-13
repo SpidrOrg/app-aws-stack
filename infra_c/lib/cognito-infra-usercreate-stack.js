00f5277f01133d94fe68a4b8ca3f0c63eaf9226f
visdxebia <vishal.daga@xebia.com>
2023-05-13 23:23:00 +0530

    sdf

---
+++ infra_c/lib/cognito-infra-usercreate-stack.js
@@ -0,0 +1,10 @@
+const {Stack} = require("aws-cdk-lib");
+
+class CognitoInfraStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+    this.stackExports = {};
+
+
+  }
+}
--- infra_c/prepareDashboardsUpload.js
+++ infra_c/prepareDashboardsUpload.js
@@ -66,7 +66,17 @@ clientsToOnboardConfigs.forEach((entity, iter) => {
   fs.cpSync(sourcePath, pathToDashboardsBundles, {recursive: true});
   fs.writeFileSync(`${pathToDashboardsBundles}/idpConfig.js`, idpConfigTemplateContents);
 
-  exec(`aws s3api head-object --bucket ${dashboardsBucketName} --key dashboards/${clientId}/dashboard/index.html`, (notExists)=>{
+  exec(`aws s3api head-object --bucket ${dashboardsBucketName} --key dashboards/${clientId}/dashboard/index.html`, (notExists, exists)=>{
+    console.log("exists", exists);
+    if (exists === null){
+      console.log("exists is real nulll")
+    }
+    if (exists){
+      console.log("exists is truthy")
+    }
+    if (!exists){
+      console.log("exists is falsy")
+    }
     if (notExists !== null){
       exec(`aws s3 cp ${pathToDashboardsTempDir} s3://${dashboardsBucketName}/dashboards --recursive`, (err, output) => {
         if (err) {
