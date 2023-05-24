7977f21bd92e859eea649ab4bd9c732d948e4d44
visdxebia <vishal.daga@xebia.com>
2023-05-24 14:54:59 +0530

    sdf

--- infra_c/lib/utils/getServiceName.js
+++ infra_c/lib/utils/getServiceName.js
@@ -1,17 +1,20 @@
+const getAWSAccountAndRegion = require("../../getAWSAccountAndRegion");
 const MODULE_PREFIX = "";
 const PREFIX = 'krny';
 const PREFIX1 = 'spi'
+
+const {awsAccount} = getAWSAccountAndRegion();
 
 function _addModulePrefix(val){
   return `${MODULE_PREFIX ? `${MODULE_PREFIX}-`: ''}${val}`
 }
 
 function getClientBucketName(clientID, env){
-  return _addModulePrefix(`${PREFIX}-${PREFIX1}-${clientID}-${env}`)
+  return _addModulePrefix(`${PREFIX}-${PREFIX1}-${clientID}${env ? `-${env}` : ''}`)
 }
 
 function getDashboardsBucketName(env){
-  return _addModulePrefix(`${PREFIX}-${PREFIX1}-dashboards-${env}`)
+  return _addModulePrefix(`${PREFIX}-${PREFIX1}-${awsAccount}-dashboards${env ? `-${env}` : ''}`)
 }
 
 module.exports = {
